f813e29bc43faf32ff50f293f517240859472967
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-08-14 18:25:30 -0700

    A Testing Module for Stack Objects.

--- stack.js
+++ stack.js
@@ -11,7 +11,12 @@ function Stack() {
 	this.array = [];
 	this.top = null;
 	this.push = function(el) {
-		this.array.push(el);
+		var newArray = [];
+		newArray.push(el);
+		for (var i=1; i<=this.len(); i++) {
+			newArray.push(this.array[i-1]);
+		}
+		this.array = newArray;
 		this.top = this.array[0];
 		return;
 	}
@@ -49,25 +54,33 @@ function Stack() {
 		}
 	}
   this.popAll = function() {
-  	for (var count=1; count < this.len(); count++) {
+  	var l = this.len();
+  	for (var count=0; count < l; count++) {
   		this.pop();
   	}
   	return;
   }
 }
 
+/*
+	Function: testing that the module works correctly
+
 var temp = new Stack();
+temp.push(1);
+temp.push(2);
+temp.push(3);
 temp.push(4);
-temp.push(4);
-temp.push(4);
-temp.push(4);
-temp.push(4);
-temp.push(4);
-temp.push(4);
-temp.push(4);
+temp.push(5);
+temp.push(6);
+temp.push(7);
+temp.push(8);
+
+temp.print();
 
 temp.popAll();
+console.log("After popAll():");
+temp.print();
 
 console.log(temp.isEmpty());
-
+*/
 
